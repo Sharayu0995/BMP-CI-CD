bcc8f570a2db15eaf30b0ad08925c5edf43190d2
sharayu bhosale <sharayu.bhosale.think@gmail.com>
2020-08-26 20:47:20 +0530

    Create Test Suite script

---
+++ src/test/java/com/bmp/Regression/CreateTestSuiteTest.java
@@ -0,0 +1,29 @@
+package com.bmp.Regression;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.bmp.Library.AppLibrary;
+import com.bmp.Library.TestBase;
+import com.bmp.PageObject.CreateNewTestPage;
+import com.bmp.PageObject.CreateTestSuite;
+import com.bmp.PageObject.LoginPage;
+
+public class CreateTestSuiteTest extends TestBase
+{
+	@BeforeClass
+	public void setUp() throws Exception {
+		appLibrary = new AppLibrary("DuplicateFunTest");
+	}
+	@Test
+	public void createTestSuite() throws Exception
+	{
+		appLibrary.getDriverInstance();
+		appLibrary.launchApp();
+		String testSuiteName="Test_Suite_"+AppLibrary.randIntDigits(0, 999);
+		String testName="My IOS Test_"+AppLibrary.randIntDigits(0, 999);
+		new LoginPage(appLibrary).login("qa638@mailinator.com", "test12");
+		new CreateNewTestPage(appLibrary).newTest(testName);
+		new CreateTestSuite(appLibrary).newTestSuite(testSuiteName, testName);
+	}
+}
